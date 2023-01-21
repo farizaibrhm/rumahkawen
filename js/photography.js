efd4914799a022bbbc1095aa7f9c3028d1a77b7d
Muhammad Najmuddin <muhammadnajmuddin1907@gmail.com>
2023-01-21 19:21:55 +0800

    calculate total

--- js/photography.js
+++ js/photography.js
@@ -1,31 +1,35 @@
 
 		function calculateTotalPhotography()
-{
-  let unit_price={
-    bp: 5500,
-    sp: 6500,
-    gp: 8500
-  };
-  let item_price={}
-  
-  item_price.bp = ($("#qty_bp").val() * unit_price.bp )
-  $("#price_bp").val(item_price.bp);
-  
-  item_price.sp = ($("#qty_sp").val() * unit_price.sp )
-  $("#price_sp").val(item_price.sp);
-  
-  item_price.gp = ($("#qty_gp").val() * unit_price.gp)
-  $("#price_gp").val(item_price.gp);  
+    {
+      let unit_price={
+        bp: 5500,
+        sp: 6500,
+        gp: 8500
+      };
+      let item_price={}
+      
+      item_price.bp = ($("#qty_bp").val() * unit_price.bp )
+      $("#price_bp").val(item_price.bp);
+      
+      item_price.sp = ($("#qty_sp").val() * unit_price.sp )
+      $("#price_sp").val(item_price.sp);
+      
+      item_price.gp = ($("#qty_gp").val() * unit_price.gp)
+      $("#price_gp").val(item_price.gp);  
+        
+      
+      let total = item_price.bp + item_price.sp + item_price.gp ;
     
-  
-  let total = item_price.bp + item_price.sp + item_price.gp ;
-
- 
-  $("#total_value").text(total);
-  
-}
-
-$(function()
- {
-    $(".qty").on("change keyup",calculateTotalPhotography)
-})
+     
+      $("#total_value").text(total);
+    
+      return total;
+      
+    }
+    
+    $(function()
+     {
+        $(".qty").on("change keyup",calculateTotalPhotography)
+    })
+    
+    
--- js/weddingdress.js
+++ js/weddingdress.js
@@ -1,23 +1,25 @@
 
 		function calculateTotalWeddingDress()
-        {
-          let unit_price={
-            wd: 1500
-          };
-          let item_price={}
-          
-          item_price.wd = ($("#qty_wd").val() * unit_price.wd )
-          $("#price_wd").val(item_price.wd);
-            
-          
-          let total = item_price.wd;
+    {
+      let unit_price={
+        wd: 1500
+      };
+      let item_price={}
+      
+      item_price.wd = ($("#qty_wd").val() * unit_price.wd )
+      $("#price_wd").val(item_price.wd);
         
-          $("#total_valuedress").text(total);
-          
-        }
-        
-        $(function()
-         {
-            $(".qty").on("change keyup",calculateTotalWeddingDress)
-        })
-        
+      
+      let total = item_price.wd;
+    
+      $("#total_valuedress").text(total);
+
+      return total;
+      
+    }
+    
+    $(function()
+     {
+        $(".qty").on("change keyup",calculateTotalWeddingDress)
+    })
+    
